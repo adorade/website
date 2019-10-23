ac5f81ee3b5debab94e9eb5f710225b3231962f3
Marian Bolojan <office.adorade@gmail.com>
2019-10-23 16:24:34 +0300

    perf(color): add transition to switcher

--- src/mjs/color-scheme.mjs
+++ src/mjs/color-scheme.mjs
@@ -25,8 +25,16 @@
   function switchTheme (event) {
     const schema = event.target.value
 
+    // Add this class tactically a moment before changing the theme
+    $('html').addClass('color-theme-in-transition')
+
     // Change color schema
     colorScheme.scheme = schema
+
+    // Remove as soon as the transition was over
+    setTimeout(() => {
+      $('html').removeClass('color-theme-in-transition')
+    }, 1000)
 
     // Store the user preference for future visits
     localStorage.setItem('theme', schema)
