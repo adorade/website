4a07f7743f087e9d136c42068fab4b1366c2fb52
Marian Bolojan <office.adorade@gmail.com>
2019-10-23 12:58:10 +0300

    perf(color): save user preference

--- src/mjs/color-scheme.mjs
+++ src/mjs/color-scheme.mjs
@@ -4,16 +4,39 @@
 /* globals initPrefersColorScheme */
 
 (() => {
-  // this enables prefers-color-scheme media queries
+  // This enables prefers-color-scheme media queries
   const colorScheme = initPrefersColorScheme('dark')
 
-  const toggler = $('.btn-group-toggle input:radio')
-  toggler.on('change', (event) => {
-    // console.log('Your schema is: ' + event.target.value)
+  // Toggle selector and currnt theme constant
+  const $toggler = $('.btn-group-toggle input:radio')
+  const currentTheme = localStorage.getItem('theme')
+
+  // Check for saved user preference, if any, on load of the website
+  if (currentTheme) {
+    console.log('Your current theme is: ' + currentTheme)
+    colorScheme.scheme = currentTheme
+
+    if (currentTheme === 'light') {
+      $toggler.first().parent().removeClass('active')
+      $toggler.last().parent().addClass('active')
+    }
+  }
+
+  // Switch theme function
+  function switchTheme (event) {
+    console.log( $( this ).val() )
+    console.log('Your schema is: ' + event.target.value)
     const schema = event.target.value
 
+    // Change color schema
     colorScheme.scheme = schema
-  })
+
+    // Store the user preference for future visits
+    localStorage.setItem('theme', schema)
+  }
+
+  // Event handlers on change
+  $toggler.change(switchTheme)
 
   // let’s run it now
   // colorScheme.onChange();
