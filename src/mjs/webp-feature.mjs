22079284cbb00faec62b229d2af8b3fa46237342
Marian Bolojan <office.adorade@gmail.com>
2019-11-19 14:59:38 +0200

    feat(webp): add polyfill for .webp image format

---
+++ src/mjs/webp-feature.mjs
@@ -0,0 +1,13 @@
+//
+// Enable webp feature (polyfill)
+// -----------------------------------------------------------------------------
+
+(() => {
+  let i = new Image
+
+  i.onload = i.onerror = () => {
+    document.body.classList.add(i.height == 1 ? 'webp' : 'no-webp')
+  }
+
+  i.src='data:image/webp;base64,UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA=='
+})()
