18a7075aa111d8fac044c799ff7bf12ef5d651a7
Marian Bolojan <office.adorade@gmail.com>
2019-12-05 00:09:17 +0200

    refactor(consent): change consent style

--- src/mjs/cookie-init.mjs
+++ src/mjs/cookie-init.mjs
@@ -1,15 +1,6 @@
 // cookie-consent init
 window.addEventListener('load', () => {
   window.cookieconsent.initialise({
-    palette: {
-      popup: {
-        background: '#252e39'
-      },
-      button: {
-        background: '#17a2b8',
-        text: '#000000'
-      }
-    },
     theme: 'classic',
     type: 'opt-out'
   })
