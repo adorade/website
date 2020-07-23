a97bf6d7aba099e4badfdbafb560e08347e7d131
Marian Bolojan <office.adorade@gmail.com>
2020-07-23 21:27:03 +0300

    refactor(lazy): add fade effect to images

--- src/mjs/lazy-images.mjs
+++ src/mjs/lazy-images.mjs
@@ -2,27 +2,67 @@
 // Lazy-load images
 // -----------------------------------------------------------------------------
 
+document.addEventListener('DOMContentLoaded', () => {
+  const cardPictures = document.querySelectorAll('.card-picture')
+  const featurePictures = [].slice.call(document.querySelectorAll('.lazy-features'))
+  const topPictures = document.querySelectorAll('.lazy-top')
 
-if ('loading' in HTMLImageElement.prototype) {
-  const images = document.querySelectorAll('img[loading="lazy"]')
-  images.forEach(img => {
-    img.src = img.dataset.src
+  topPictures.forEach(top => {
+    top.parentNode.classList.add('fade-in')
   })
 
-  const cardsPicture = document.querySelectorAll('.card-picture')
-  cardsPicture.forEach(card => {
-    card.parentNode.classList.add('card-fade-in')
-  })
+  if ('IntersectionObserver' in window) {
+    let cardImageObserver = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          let cardImage = entry.target
+          cardImage.parentNode.classList.add('card-fade-in')
+          cardImageObserver.unobserve(cardImage)
+        }
+      })
+    }, {
+      threshold: 0.3
+    })
 
-  const fadeImages = [].slice.call(document.querySelectorAll(
-    '.lazy-top, .lazy-features'
-  ))
-  fadeImages.forEach(img => {
-    img.parentNode.classList.add('fade-in')
-  })
-} else {
-  // Dynamically import the LazySizes library
-  const script = document.createElement('script')
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/lazysizes.min.js'
-  document.body.appendChild(script)
-}
+    let featImageObserver = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          let featImage = entry.target
+          featImage.parentNode.classList.add('fade-in')
+          featImageObserver.unobserve(featImage)
+        }
+      })
+    }, {
+      threshold: 0.3
+    })
+
+    cardPictures.forEach(card => {
+      cardImageObserver.observe(card)
+    })
+
+    featurePictures.forEach(feat => {
+      featImageObserver.observe(feat)
+    })
+  } else {
+    cardPictures.forEach(card => {
+      card.parentNode.classList.add('card-fade-in')
+    })
+
+    featurePictures.forEach(feat => {
+      feat.parentNode.classList.add('fade-in')
+    })
+  }
+
+  if ('loading' in HTMLImageElement.prototype) {
+    const images = document.querySelectorAll('img[loading="lazy"]')
+    images.forEach(img => {
+      img.src = img.dataset.src
+    })
+  } else {
+    // Dynamically import the LazySizes library
+    let script = document.createElement('script')
+    script.async = true
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/lazysizes.min.js'
+    document.body.appendChild(script)
+  }
+})
