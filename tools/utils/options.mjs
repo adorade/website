94b260ddf4722d54ada379062eefd8a1adb86ec4
Marian Bolojan <office.adorade@gmail.com>
2023-04-07 18:13:53 +0300

    build(tools): add stylelint quiet deprecation

--- tools/utils/options.mjs
+++ tools/utils/options.mjs
@@ -15,7 +15,8 @@ export const opts = {
     reportOutputDir: paths.logs.gulp,
     reporters: [
       { formatter: 'string', console: true, save: 'styles.txt' }
-    ]
+    ],
+    quietDeprecationWarnings: isProd ? true : false
   },
   sass: {
     outputStyle: 'expanded',
