9c55826d0aad46370715b1fcbe745f455e4b6132
Marian Bolojan <office.adorade@gmail.com>
2023-10-01 21:48:20 +0300

    refactor(scroll): update on scroll event

--- src/mjs/doc-ready.mjs
+++ src/mjs/doc-ready.mjs
@@ -2,65 +2,157 @@
 // Document ready
 // =============================================================================
 
-// Prepare hero height and reset on orientation change and window resize
+// Prepare hero height on load and reset on resize
 // -----------------------------------------------------------------------------
-$(window).on('load orientationchange resize', () => {
-  $('.hero-unit').height( $(window).height() )
+window.onload = window.onresize = () => {
+  const heroUnit = document.querySelector('.hero-unit')
+  if (heroUnit)
+    heroUnit.style.height = window.innerHeight + 'px'
+}
+
+// On Document Ready
+// -----------------------------------------------------------------------------
+function ready (fn) {
+  if (document.readyState != 'loading') {
+    fn()
+  } else {
+    document.addEventListener('DOMContentLoaded', fn)
+  }
+}
+
+// Scroll to Next section
+// -----------------------------------------------------------------------------
+ready(() => {
+  const scrollDown = document.querySelector('.scroll-down')
+
+  if (scrollDown)
+    scrollDown.onclick = () => {
+      const targetElement = document.querySelector('#cards')
+      const targetOffset = targetElement.offsetTop
+
+      window.scrollTo({
+        top: targetOffset,
+        behavior: 'smooth'
+      })
+    }
+})
+
+// Back to Top
+// -----------------------------------------------------------------------------
+ready(() => {
+  document.querySelector('.back-to-top-icon').onclick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
 })
 
 // On Document Ready
 // -----------------------------------------------------------------------------
-$(() => {
+ready(() => {
   // Parallax Effect
   // ------------------------
   $('.parallax-img').parallax()
-
-  // Scroll to Cards section
-  // ------------------------
-  $('.scroll-down').on('click', () => {
-    $('html, body').animate({
-      // scrollTop: $('#cards').offset().top - 56 // value for .fixed-top
-      scrollTop: $('#cards').offset().top
-    }, 1000)
-  })
-
-  // Back to Top
-  // ------------------------
-  $('.back-to-top-icon').on('click', () => {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1000)
-  })
 })
 
 // On Window Scroll
 // -----------------------------------------------------------------------------
-$(window).on('scroll', function () {
-  const $windowPos = $(this).scrollTop(),
-        $windowH = $(this).height(),
-        $documentH = $(document).height(),
-        $toggler = $('.btn-toggler'),
-        $getInTouch = $('.get-in-touch'),
-        $totop = $('.back-to-top'),
-        $percent = $('.back-to-top-percent'),
-        $hero = $('.hero-unit').height() - 1,
-        $masthead = $('.masthead').height() - 1,
-        scrollPos = $windowPos ? $windowPos : 0
+window.onscroll = () => {
+  const windowPos = window.scrollY,
+        windowH = window.innerHeight,
+        documentH = document.documentElement.scrollHeight,
+        scrollPos = windowPos ? windowPos : 0,
 
-  if (scrollPos > $hero || scrollPos > $masthead) {
-    $toggler.addClass('btn-orange')
-    $getInTouch.addClass('shrink')
-    $totop.filter(':hidden').stop(true, true).fadeIn()
-  } else {
-    $toggler.removeClass('btn-orange')
-    $getInTouch.removeClass('shrink')
-    $totop.stop(true, true).fadeOut()
+        togglers = document.querySelectorAll('.btn-toggler'),
+        getInTouch = document.querySelector('.get-in-touch'),
+        hero = document.querySelector('.hero-unit'),
+        masthead = document.querySelector('.masthead'),
+        totop = document.querySelector('.back-to-top'),
+        percent = document.querySelector('.back-to-top-percent')
+
+  let heroH, mastheadH
+  if (hero) heroH = hero.clientHeight - 1
+  if (masthead) mastheadH = masthead.clientHeight - 1
+
+  const fadeIn = (el, smooth = true, displayStyle = 'block') => {
+    el.style.opacity = 0
+    el.style.display = displayStyle
+
+    if (smooth) {
+      let opacity = 0
+      let request
+
+      const animation = () => {
+        opacity += 0.04
+        if (opacity >= 1) {
+          opacity = 1
+          cancelAnimationFrame(request)
+        }
+        el.style.opacity = opacity
+      }
+
+      const rAf = () => {
+        request = requestAnimationFrame(rAf)
+        animation()
+      }
+
+      rAf()
+
+    } else {
+      el.style.opacity = 1
+    }
   }
 
-  let scrollProcent = 0
-  scrollProcent = 100 - Math.round(
-    (($documentH - $windowH - $windowPos) * 100) /
-    ($documentH - $windowH)
-  )
-  $percent.height(scrollProcent + '%')
-})
+  const fadeOut = (el, smooth = true, displayStyle = 'none') => {
+    if (smooth) {
+      let opacity = el.style.opacity
+      let request
+
+      const animation = () => {
+        opacity -= 0.04
+        if (opacity <= 0) {
+          opacity = 0
+          cancelAnimationFrame(request)
+        }
+        el.style.opacity = opacity
+        el.style.display = displayStyle
+      }
+
+      const rAf = () => {
+        request = requestAnimationFrame(rAf)
+        animation()
+      }
+
+      rAf()
+
+    } else {
+      el.style.opacity = 0
+      el.style.display = displayStyle
+    }
+  }
+
+  if (scrollPos > heroH || scrollPos > mastheadH) {
+    togglers.forEach((toggler) => {
+      toggler.classList.add('btn-orange')
+    })
+    getInTouch.classList.add('shrink')
+    if (totop.style.display !== 'block') {
+      fadeIn(totop)
+    }
+  } else {
+    togglers.forEach((toggler) => {
+      toggler.classList.remove('btn-orange')
+    })
+    getInTouch.classList.remove('shrink')
+    if (totop.style.display !== 'none') {
+      fadeOut(totop)
+    }
+  }
+
+  let scrollPercent = 0
+  scrollPercent = 100 - Math.round(
+    ((documentH - windowH - windowPos) * 100) /
+    (documentH - windowH))
+  percent.style.height = scrollPercent + '%'
+}
