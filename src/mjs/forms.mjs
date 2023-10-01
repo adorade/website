cdc155ba2a2ca1b9757d13cff0ed190e1c55bbdd
Marian Bolojan <office.adorade@gmail.com>
2023-10-01 20:35:15 +0300

    refactor(form): update forms dark mode

--- src/mjs/forms.mjs
+++ src/mjs/forms.mjs
@@ -1,5 +1,5 @@
 //
-// AJAX Form Submissions
+// Form Submissions
 // -----------------------------------------------------------------------------
 
 // Disabling form submissions if there are invalid fields
@@ -7,78 +7,119 @@
 // each <form> element must have the `novalidate` attribute and at the same time,
 // each <input> element in it must have the `required` attribute.
 // -----------------------------------------------------------------------------
-$(window).on('load', () => {
-
+window.addEventListener('load', function () {
   // --- Grab all the forms
-  let form = $('.needs-validation')
+  const forms = document.querySelectorAll('.needs-validation')
+  // const modalQuote = document.querySelector('#modal-get-quote').closest('.modal-dialog')
 
-  // --- Attach a submit handler to the form
-  $(form).on('submit', function (event) {
+  // --- Attach a submit handler to each form
+  forms.forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      // --- Check form validity
+      if (!form.checkValidity()) {
+        // --- Stop form from submitting normally, handle the invalid form...
+        event.preventDefault()
+        event.stopPropagation()
 
-    // --- Check form validity
-    if (this.checkValidity() == false) {
-      // --- Stop form from submitting normally, handle the invalid form...
-      event.preventDefault()
-      event.stopPropagation()
+        // --- Add `shake` animation
+        form.classList.add('shake-x', 'animated')
+        form.addEventListener('animationend', function () {
+          form.classList.remove('shake-x', 'animated')
+        }, { once: true })
 
-      // --- Add `shake` animation
-      $(this).addClass('shake-x animated').one('animationend', function () {
-        $(this).removeClass('shake-x animated')
-      })
-    } else {
-      // --- Since form is now valid, prevent default behavior.
-      event.preventDefault()
-
-      // --- Get some values from elements on the page:
-      const $form = $(this),
-            url = $form.attr('action'),
-            datas = $form.serialize(),
-            $response = $form.parent().find('.form-response')
-
-      // --- Message that will be displayed when everything is OK :)
-      let okMessage
-      if ( $form.attr('id') === 'news-form' ) {
-        okMessage = 'We have sent an email to confirm your subscription to your email address. Thank you for subscribing!'
+        // --- Change modal-dialog shadow
+        // if (modalQuote) {
+        //   console.log('EROOOOR')
+        //   modalQuote.classList.add('modal-quote-error')
+        // }
       } else {
-        okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!'
+        // --- Since the form is now valid, prevent the default behavior.
+        event.preventDefault()
+
+        // --- Get some values from elements on the page:
+        const url = form.getAttribute('action')
+        const data = new URLSearchParams(new FormData(form))
+        const response = form.parentElement.querySelector('.form-response')
+
+        // --- Message that will be displayed when everything is OK :)
+        let okMessage
+        if (form.getAttribute('id') === 'news-form') {
+          okMessage = 'We have sent an email to confirm your subscription to your email address. Thank you for subscribing!'
+        } else {
+          okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!'
+        }
+
+        // --- If something goes wrong, we will display this message. :(
+        const errorMessage = 'There was an error while submitting the form. Please try again later'
+
+        // --- Send the data using post
+        fetch(url, {
+          method: 'POST',
+          body: data
+        })
+          .then(function (response) {
+            if (response.ok) {
+              return response.text()
+            } else {
+              throw new Error('Network response was not ok')
+            }
+          })
+          .then(function () {
+            response.classList.add('alert-success')
+            response.classList.remove('d-none')
+            response.textContent = okMessage
+          })
+          .catch(function () {
+            response.classList.add('alert-danger')
+            response.classList.remove('d-none')
+            response.textContent = errorMessage
+          })
+          .finally(function () {
+            setTimeout(function () {
+              response.style.display = 'none'
+
+              setTimeout(function () {
+                if (response.classList.contains('alert-success') || response.classList.contains('alert-danger')) {
+                  response.classList.remove('alert-success', 'alert-danger')
+                  response.classList.add('d-none')
+                  response.textContent = ''
+                }
+              }, 1000)
+            }, 8000)
+
+            // Trigger blur event on submit button
+            var submitButton = form.querySelector('button[type="submit"]')
+            if (submitButton) {
+              submitButton.blur()
+            }
+
+            // Reset form
+            form.classList.remove('was-validated')
+            form.reset()
+          })
       }
 
-      // --- If something goes wrong, we will display this message. :(
-      const errorMessage = 'There was an error while submitting the form. Please try again later'
+      // Add 'was-validated' class to the form
+      form.classList.add('was-validated')
 
-      // --- Send the data using post
-      $.post(url, datas)
-        .done(() => {
-          $response.addClass('alert-success').removeClass('d-none').append(okMessage)
-        })
-        .fail(() => {
-          $response.addClass('alert-danger').removeClass('d-none').append(errorMessage)
-        })
-        .always(() => {
-          setTimeout(() => {
-            $response.fadeOut()
+      // Trigger blur event on submit button
+      const submitButton = form.querySelector('button[type="submit"]')
+      if (submitButton) {
+        submitButton.blur()
+      }
+    })
 
-            setTimeout(() => {
-              if ( $response.hasClass('alert-success') || $response.hasClass('alert-danger') ) {
-                $response
-                  .removeClass('alert-success alert-danger')
-                  .addClass('d-none').removeAttr('style').empty()
-              }
-            }, 1000)
-          }, 8000)
-
-          $('button:submit').trigger('blur')
-          $form.removeClass('was-validated').trigger('reset')
-        })
+    // --- Reset form
+    const resetButton = form.querySelector('button[type="reset"]')
+    if (resetButton) {
+      resetButton.addEventListener('click', function () {
+        form.classList.remove('was-validated')
+        // if (modalQuote) {
+        //   console.log('REMOVED')
+        //   modalQuote.classList.remove('modal-quote-error')
+        // }
+        resetButton.blur()
+      })
     }
-
-    $(this).addClass('was-validated')
-    $('button:submit').trigger('blur')
-  })
-
-  // --- Reset form
-  $('button:reset').on('click', function () {
-    $(form).removeClass('was-validated')
-    $(this).trigger('blur')
   })
 })
